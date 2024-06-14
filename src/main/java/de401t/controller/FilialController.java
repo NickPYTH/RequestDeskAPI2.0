ba1067affdf1cab4afefb7478bdff5eba2555ecf
DirectumRXUser <directumrxuser@c.ru>
2024-06-14 13:16:30 +0300

    upd5

--- src/main/java/de401t/controller/FilialController.java
+++ src/main/java/de401t/controller/FilialController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/filials")
+@RequestMapping("/apiNew/filials")
 @Api(tags = "filials")
 @RequiredArgsConstructor
 public class FilialController {
